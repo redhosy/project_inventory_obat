ac8faeb793f0cff57d6c50438c0d411f8508a82d
redhosy <127164173+redhosy@users.noreply.github.com>
2024-11-07 23:27:28 +0700

    Menambahkan folder dan perubahan terbaru

---
+++ frontend/src/components/Sidebar/index.tsx
@@ -0,0 +1,242 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import SidebarItem from "../../components/Sidebar/SidebarItem";
+import ClickOutside from "../../components/ClickOutside";
+import useLocalStorage from "../../hooks/useLocalStorage";
+
+interface SidebarProps {
+  sidebarOpen: boolean;
+  setSidebarOpen: (arg: boolean) => void;
+}
+
+const menuGroups = [
+  {
+    name: "MENU",
+    menuItems: [
+      {
+        icon: (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 512 512">
+            <path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm320 96c0-26.9-16.5-49.9-40-59.3L280 88c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 204.7c-23.5 9.5-40 32.5-40 59.3c0 35.3 28.7 64 64 64s64-28.7 64-64zM144 176a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm-16 80a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM400 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z" /></svg>
+        ),
+        label: "Dashboard",
+        route: "/",
+      },
+      {
+        icon: (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 512 512">
+            <path d="M448 160l-128 0 0-32 128 0 0 32zM48 64C21.5 64 0 85.5 0 112l0 64c0 26.5 21.5 48 48 48l416 0c26.5 0 48-21.5 48-48l0-64c0-26.5-21.5-48-48-48L48 64zM448 352l0 32-256 0 0-32 256 0zM48 288c-26.5 0-48 21.5-48 48l0 64c0 26.5 21.5 48 48 48l416 0c26.5 0 48-21.5 48-48l0-64c0-26.5-21.5-48-48-48L48 288z" /></svg>
+        ),
+        label: "Management Obat",
+        route: "/ManagementObat",
+      },
+      {
+        icon: (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 512 512">
+            <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z" /></svg>
+        ),
+        label: "Transaksi Obat Masuk",
+        route: "/TransaksiObatMasuk",
+      },
+      {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 512 512"
+            style={{ transform: "scaleX(-1)" }}
+          >
+            <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z" />
+          </svg>
+        ),
+        label: "Transaksi Obat Keluar",
+        route: "/TransaksiObatKeluar",
+      },
+      {
+        icon: (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 640 512">
+            <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192l42.7 0c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0L21.3 320C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7l42.7 0C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3l-213.3 0zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352l117.3 0C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7l-330.7 0c-14.7 0-26.7-11.9-26.7-26.7z" /></svg>
+        ),
+        label: "Data Suppliers",
+        route: "/Supplier",
+      },
+      {
+        icon: (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 640 512">
+            <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192l42.7 0c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0L21.3 320C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7l42.7 0C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3l-213.3 0zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352l117.3 0C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7l-330.7 0c-14.7 0-26.7-11.9-26.7-26.7z" /></svg>
+        ),
+        label: "Data Pelanggan",
+        route: "/Pelanggan",
+      },
+      {
+        icon: (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 384 512">
+            <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 288c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128z" />
+          </svg>
+        ),
+        label: "Laporan Obat",
+        route: "#",
+        subItems: [
+          { label: "Laporan Obat Masuk", route: "/LaporanObatMasuk" },
+          { label: "Laporan Obat Keluar", route: "/LaporanObatKeluar" },
+          { label: "Laporan Management Stok", route: "/LaporanManagementStok"},
+          { label: "Laporan Penjualan", route: "/LaporanPenjualan"},
+        ],
+      },
+      {
+        icon: (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="fill-current"
+            width="18"
+            height="18"
+            viewBox="0 0 512 512">
+            <path d="M0 96C0 60.7 28.7 32 64 32l384 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zm64 0l0 64 64 0 0-64L64 96zm384 0L192 96l0 64 256 0 0-64zM64 224l0 64 64 0 0-64-64 0zm384 0l-256 0 0 64 256 0 0-64zM64 352l0 64 64 0 0-64-64 0zm384 0l-256 0 0 64 256 0 0-64z" /></svg>
+        ),
+        label: "Management Stok",
+        route: "/ManagementStok",
+      },
+    ],
+  },
+];
+
+const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
+  const pathname = usePathname();
+  const [pageName, setPageName] = useLocalStorage("selectedMenu", "dashboard");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <ClickOutside onClick={() => setSidebarOpen(false)}>
+      <aside
+        className={`fixed left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-teal-900 duration-300 ease-linear dark:bg-boxdark lg:translate-x-0 ${sidebarOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+      >
+        {/* <!-- SIDEBAR HEADER --> */}
+        <div className="flex items-center justify-center gap-2 px-6 py-5.5 lg:py-6.5">
+          <Link href="/">
+            <Image
+              width={176}
+              height={36}
+              src={"/images/logo/logo.png"}
+              alt="Logo"
+              priority
+            />
+          </Link>
+
+          <button
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+            aria-controls="sidebar"
+            className="block lg:hidden"
+          >
+            <svg
+              className="fill-current"
+              width="20"
+              height="18"
+              viewBox="0 0 20 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 8.175H2.98748L9.36248 1.6875C9.69998 1.35 9.69998 0.825 9.36248 0.4875C9.02498 0.15 8.49998 0.15 8.16248 0.4875L0.399976 8.3625C0.0624756 8.7 0.0624756 9.225 0.399976 9.5625L8.16248 17.4375C8.31248 17.5875 8.53748 17.7 8.76248 17.7C8.98748 17.7 9.17498 17.625 9.36248 17.475C9.69998 17.1375 9.69998 16.6125 9.36248 16.275L3.02498 9.8625H19C19.45 9.8625 19.825 9.4875 19.825 9.0375C19.825 8.55 19.45 8.175 19 8.175Z"
+                fill=""
+              />
+            </svg>
+          </button>
+        </div>
+        {/* <!-- SIDEBAR HEADER --> */}
+        <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
+
+          {/* <!-- Sidebar Menu --> */}
+          <nav className="mt-4 px-4 py-4 lg:mt-5 lg:px-6">
+            {menuGroups.map((group, groupIndex) => (
+              <div key={groupIndex}>
+                <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
+                  {group.name}
+                </h3>
+
+                <ul className="mb-6 flex flex-col gap-1.5">
+                  {group.menuItems.map((menuItem, menuIndex) => (
+                    <li key={menuIndex}>
+                      {menuItem.subItems ? (
+                        <div>
+                          <button
+                            className="flex w-full text-white items-center gap-2 py-2 px-4 text-left"
+                            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                          >
+                            {menuItem.icon}
+                            <span>{menuItem.label}</span>
+                            <svg
+                              className={`ml-auto transform ${isDropdownOpen ? "rotate-180" : "rotate-0"}`}
+                              width="12"
+                              height="12"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                            >
+                              <path fillRule="evenodd" d="M5.293 9.293a1 1 0 011.414 0L10 12.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                            </svg>
+                          </button>
+                          {isDropdownOpen && (
+                            <ul className="pl-8">
+                              {menuItem.subItems.map((subItem, subIndex) => (
+                                <li key={subIndex}>
+                                  <Link
+                                    href={subItem.route}
+                                    className="block py-2 px-4 text-white hover:bg-gray-800"
+                                  >
+                                    {subItem.label}
+                                  </Link>
+                                </li>
+                              ))}
+                            </ul>
+                          )}
+                        </div>
+                      ) : (
+                        <SidebarItem
+                          key={menuIndex}
+                          item={menuItem}
+                          pageName={pageName}
+                          setPageName={setPageName}
+                        />
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </nav>
+          {/* <!-- Sidebar Menu --> */}
+        </div>
+      </aside>
+    </ClickOutside>
+  );
+};
+
+export default Sidebar;
